2bcb5dbd51bdc8752c074bc27ec93831afd45de4
Ander <ander@protonmail.com>
2022-01-29 17:12:00 +0100

    Updates personal info

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   useKatex: false,
   author: {
     name: `Ander Granado`,
-    summary: `, Ingeniero informático, desarrollador de software y hacker`,
+    summary: `, Hacker, Cybersecurity Consultant & Software Engineer`,
     social: {
       twitter: `andergrma`,
       github: `ander94lakx`,
